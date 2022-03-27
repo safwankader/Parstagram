312a4ffb345a85af60ea2b02123d8f792e09a6fb
safwankader <skaderny@gmail.com>
2022-03-27 15:10:04 -0400

    Initial commit

---
+++ app/src/main/java/com/safwan/parstagram/LoginActivity.kt
@@ -0,0 +1,78 @@
+package com.safwan.parstagram
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.parse.ParseUser
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Check if there's a user logged in
+        // If there is, take them to MainActivity
+
+        if(ParseUser.getCurrentUser() != null) {
+            goToMainActivity()
+        }
+
+        findViewById<Button>(R.id.login_button).setOnClickListener {
+            val username = findViewById<EditText>(R.id.et_username).text.toString()
+            val password = findViewById<EditText>(R.id.et_password).text.toString()
+            loginUser(username, password)
+        }
+
+        findViewById<Button>(R.id.signupButton).setOnClickListener {
+            val username = findViewById<EditText>(R.id.et_username).text.toString()
+            val password = findViewById<EditText>(R.id.et_password).text.toString()
+            signUpUser(username, password)
+        }
+    }
+
+    private fun signUpUser(username: String, password: String){
+        // Create the ParseUser
+        val user = ParseUser()
+
+        // Set fields for the user to be created
+        user.setUsername(username)
+        user.setPassword(password)
+
+        user.signUpInBackground { e ->
+            if (e == null) {
+                // User has successfully signed created a new account
+
+                // TODO: Navigate user to the MainActivity
+                // TODO: Show a toast to indicate a user successfully signed up for an account
+            } else {
+                // TODO: Show a toast to tell user sign up was not successful
+                e.printStackTrace()
+            }
+        }
+    }
+    private fun loginUser(username: String, password: String) {
+        ParseUser.logInInBackground(username, password, ({ user, e ->
+            if (user != null) {
+                Log.i(TAG,"Successfully logged in user")
+                goToMainActivity()
+            } else {
+                e.printStackTrace()
+                Toast.makeText(this,"Error logging in", Toast.LENGTH_SHORT).show()
+            }})
+        )
+
+    }
+
+    private fun goToMainActivity(){
+        val intent = Intent(this@LoginActivity,MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    companion object {
+        const val TAG = "LoginActivity"
+    }
+}
---
+++ app/src/main/java/com/safwan/parstagram/Post.kt
@@ -0,0 +1,41 @@
+package com.safwan.parstagram
+
+import com.parse.ParseClassName
+import com.parse.ParseFile
+import com.parse.ParseObject
+import com.parse.ParseUser
+
+// Description : String
+// Image : File
+// User : User
+
+@ParseClassName("Post")
+class Post : ParseObject() {
+
+    fun getDescription(): String? {
+        return getString(KEY_DESCRIPTION)
+    }
+
+    fun setDescription(descrption: String){
+        put(KEY_DESCRIPTION,descrption)
+    }
+
+    fun setImage(parsefile: ParseFile) {
+        put(KEY_IMAGE,parsefile)
+    }
+
+    fun getUser() : ParseUser? {
+        return getParseUser(KEY_USER)
+    }
+
+    fun setUser(user: ParseUser) {
+        put(KEY_USER, user)
+    }
+
+    companion object{
+        const val KEY_DESCRIPTION = "description"
+        const val KEY_IMAGE = "Image"
+        const val KEY_USER = "user"
+    }
+
+}
